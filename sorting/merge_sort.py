8c94f4d44c18ec1e3ca12a3bb990d7ae9893046b
William C Grisaitis <william@grisait.is>
2019-11-14 22:51:44 -0500

    Add merge sort

---
+++ sorting/merge_sort.py
@@ -0,0 +1,28 @@
+def merge_sort(x: list):
+  n = len(x)
+  def combine(y, z):
+    i_y = i_z = 0
+    n_y, n_z = len(y), len(z)
+    result = list()
+    while i_y < n_y and i_z < n_z:
+      if y[i_y] < z[i_z]:
+        result.append(y[i_y])
+        i_y += 1
+      else:
+        result.append(z[i_z])
+        i_z += 1
+    result.extend(y[i_y:n_y])
+    result.extend(z[i_z:n_z])
+    return result
+
+  if n == 1:
+    return x
+  elif n == 2:
+    if x[0] > x[1]:
+      return x[::-1]  # reverse x
+    else:
+      return x
+  else:
+    midpoint = n // 2
+    x1, x2 = x[0:midpoint], x[midpoint:n]
+    return combine(merge_sort(x1), merge_sort(x2))
---
+++ tests/test_merge_sort.py
@@ -0,0 +1,21 @@
+import random
+
+from sorting.merge_sort import merge_sort
+
+
+class TestMergeSort:
+  def test_sorts_stuff(self):
+    stuff = [2, 4, 3, 1]
+    assert merge_sort(stuff) == [1, 2, 3, 4]
+
+  def test_sorts_big_list(self):
+    big_thing = list(range(10000))
+    random.seed(0)
+    random.shuffle(big_thing)
+    assert merge_sort(big_thing) == list(range(10000))
+
+  def test_python_sorts_big_list(self):
+    big_thing = list(range(10000))
+    random.seed(0)
+    random.shuffle(big_thing)
+    assert list(sorted(big_thing)) == list(range(10000))
