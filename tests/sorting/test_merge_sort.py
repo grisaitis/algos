8146cd69adafbd22da44a3455da2de85f9355656
William C Grisaitis <william@grisait.is>
2019-11-15 11:12:08 -0500

    Make tests/sorting dir

--- tests/sorting/test_merge_sort.py
+++ tests/sorting/test_merge_sort.py
@@ -4,7 +4,7 @@ from sorting.merge_sort import merge_sort
 
 
 class TestMergeSort:
-  def test_sorts_stuff(self):
+  def test_sorts_small_list(self):
     stuff = [2, 4, 3, 1]
     assert merge_sort(stuff) == [1, 2, 3, 4]
 
@@ -13,9 +13,3 @@ class TestMergeSort:
     random.seed(0)
     random.shuffle(big_thing)
     assert merge_sort(big_thing) == list(range(10000))
-
-  def test_python_sorts_big_list(self):
-    big_thing = list(range(10000))
-    random.seed(0)
-    random.shuffle(big_thing)
-    assert list(sorted(big_thing)) == list(range(10000))
